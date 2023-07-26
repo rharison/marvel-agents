8aa6c396ce9c3e9ce4cdc8930496be8baeca1a7e
Rharison Lucas <rharison.abreu@gmail.com>
2023-07-26 18:57:35 -0300

    feat: evolução estrutura inicial do projeto

--- marvel-agents/src/App.tsx
+++ marvel-agents/src/App.tsx
@@ -1,9 +1,23 @@
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+import { gray } from "./theme/colors";
+
+const mdTheme = createTheme(
+  {
+    palette: {
+      divider: '#EBEFF2',
+      background: {
+        default: gray['background'],
+      }
+    },
+  },
+);
+
 function App() {
-
   return (
-    <>
-
-    </>
+    <ThemeProvider theme={mdTheme}>
+      <CssBaseline />
+      <h1>MARVEL</h1>
+    </ThemeProvider>
   )
 }
 
