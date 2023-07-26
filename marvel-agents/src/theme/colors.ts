d591dda438785158a1220e3f39cf96998967dbe8
Rharison Lucas <rharison.abreu@gmail.com>
2023-07-26 18:57:20 -0300

    feat: criando arquivo de cores

---
+++ marvel-agents/src/theme/colors.ts
@@ -0,0 +1,16 @@
+export const blue = {
+    200: '#747D94',
+    600: '#213770',
+    800: '#00113D'
+}
+
+export const orange = {
+    400: '#F43724',
+    500: '#F21A05',
+}
+
+export const gray = {
+    background: '#F5F6F8',
+    100: '#EAECF0',
+    500: '#777777',
+}
