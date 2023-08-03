58a007d5e789bf62035f74d534b28bda8269f12d
Rharison Lucas <rharison.abreu@gmail.com>
2023-08-03 13:12:30 -0300

    feat: evolução layout homepage

--- marvel-agents/src/layout/Home/index.tsx
+++ marvel-agents/src/layout/Home/index.tsx
@@ -1,10 +1,18 @@
+import { ChildrenContainer, Container, Content, Header } from "./styles";
 import { LayoutHomeProps } from "./types";
-
 
 export const LayoutHome = ({ children }: LayoutHomeProps) => {
     return (
-        <>
-            {children}
-        </>
+        <Container>
+            <Header>
+                <img src="logo_pontua_white.png" alt="Logo Pontua" />
+            </Header>
+            <Content>
+                <img src="building_illustration.png" alt="Ilustração de construção civil" />
+                <ChildrenContainer>
+                    {children}
+                </ChildrenContainer>
+            </Content>
+        </Container>
     )
 };
---
+++ marvel-agents/src/layout/Home/styles.ts
@@ -0,0 +1,22 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color: #00113D;
+    padding: 49px 68px 168px 106px;
+`;
+
+export const Header = styled.header`
+    width: 100%;
+`;
+
+export const Content = styled.div`
+    margin: 42px 0 0 58px;
+`;
+
+export const ChildrenContainer = styled.div`
+    box-shadow: 0px 17px 25px 0px #00000040;
+    border-radius: 1.75rem;
+    background-color: #FFFFFF;
+`;
